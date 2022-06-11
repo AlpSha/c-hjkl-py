679a04e18331be0559ef423fdcc7015fc64d1131
Alp Sahin <alp@sahin.club>
2022-06-12 00:01:28 +0300

    Getting keyboards list

---
+++ src/enumerator.py
@@ -0,0 +1,46 @@
+from io import TextIOWrapper
+
+
+class Keyboard:
+    def __init__(self, name, path):
+        self.name = name
+        self.path = path
+
+def enumerate_keyboards():
+    try:
+        file = open('/proc/bus/input/devices', 'r')
+    except:
+        print('Unable to open /proc/bus/input/devices')
+        return
+    entries = collect_device_entries(file)
+    keyboards = []
+    for entry in entries:
+        if entry['handler'] == None:
+            continue
+        if entry['ev'] != '120013':
+            continue
+        keyboards.append(Keyboard(entry['name'], entry['handler']))
+    return keyboards
+
+
+def collect_device_entries(file: TextIOWrapper):
+        entries = []
+        entry = {}
+        for line in file:
+            if line == '\n':
+                entries.append(entry)
+                entry = {}
+                continue
+            parts = line.split('=')
+            value = parts[1].replace('"', '').strip()
+            if line.startswith('N: Name='):
+                entry['name'] = value
+            elif line.startswith('H: Handlers='):
+                allHandlers = value.split(' ')
+                entry['handler'] = next(handler for handler in allHandlers if handler.startswith('event'))
+            elif line.startswith('B: EV='):
+                entry['ev'] = value
+            else:
+                continue
+        return entries
+    

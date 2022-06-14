91b1654a5d911de7326a2ec58523e2623395af9c
Alp Sahin <alp@sahin.club>
2022-06-14 16:59:12 +0300

    Fixed the path issue with install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
-
-install "$SCRIPT_DIR/c-hjkl" /usr/local/bin
-install "$SCRIPT_DIR/systemd/c-hjkl-py.service" /etc/systemd/system
+install /opt/c-hjkl-py/c-hjkl /usr/local/bin
+install /opt/c-hjkl-py/systemd/c-hjkl-py.service /etc/systemd/system
 systemctl daemon-reload
 systemctl enable --now c-hjkl-py.service
